a2068a1981bac29bcace2159135b3627418783ec
kamweru <subtillia@gmail.com>
2021-03-02 01:35:43 +0300

    store.getters["auth/isAuthenticated"]

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,7 +1,7 @@
-export default function(context) {
-  console.log(context.store.getters["auth/isAuthenticated"]);
-  if (context.store.getters["auth/isAuthenticated"]) {
-    return context.redirect("/");
+export default function({ store, redirect }) {
+  console.log(store.getters["auth/isAuthenticated"]);
+  if (store.getters["auth/isAuthenticated"]) {
+    return redirect("/");
   }
 }
 // export default function({ $auth, redirect }) {
