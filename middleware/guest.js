9203a507403fae0374b0144d72b546122ee99b8b
kamweru <subtillia@gmail.com>
2021-03-02 00:54:36 +0300

    context.$state.$auth.loggedIn

--- middleware/guest.js
+++ middleware/guest.js
@@ -1,6 +1,6 @@
 export default function(context) {
-  if (context.app.$auth.state.loggedIn) {
-    return context.app.$auth.redirect("/");
+  if (context.$state.auth.loggedIn) {
+    return context.redirect("/");
   }
 }
 // export default function({ $auth, redirect }) {
